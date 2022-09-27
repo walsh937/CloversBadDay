c25915d2ea2f0c716f3cb23d8b200bb614c805b4
AndyP <51890861+andyjphu@users.noreply.github.com>
2022-09-26 20:18:40 -0500

    hope it dont break

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -1,23 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerData : MonoBehaviour
 {
     public int luck = 0;
-    public
+    public GameObject luckText;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        UpdateUI(); //Turn into an event based system for performance
     }
 
     public void UpdateUI()
     {
+        luckText.GetComponent<TMP_Text>().text = "Luck: " + luck.ToString();
     }
 }
