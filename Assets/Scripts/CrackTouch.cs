a848825d110a767be32ec91c421d2012f4a5c695
AndyP <andy.jiapao.phu@gmail.com>
2022-09-27 18:21:40 -0500

    Yay I fixed it
    I did a thing

---
+++ Assets/Scripts/CrackTouch.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrackTouch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+    }
+}
