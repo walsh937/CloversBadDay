cc973b3e28c09b64c92b40b0d3ff1a20649405f0
AndyP <51890861+andyjphu@users.noreply.github.com>
2022-09-29 17:21:06 -0500

    made changes to player data

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -1,23 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerData : MonoBehaviour
 {
     public int luck = 0;
-    public
+    public GameObject text;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
     }
 
     public void UpdateUI()
     {
+        text.GetComponent<TMP_Text>().text = string.Format("luck: {0}", luck);
     }
 }
