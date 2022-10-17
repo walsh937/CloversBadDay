58baad9a3ce1e9ca170188454adb3fbae74a589d
AndyP <51890861+andyjphu@users.noreply.github.com>
2022-10-17 18:02:16 -0500

    crack cocaine

--- Assets/Scripts/CrackSpawner.cs
+++ Assets/Scripts/CrackSpawner.cs
@@ -48,6 +48,7 @@ public class CrackSpawner : MonoBehaviour
                 crack_instance.transform.position += new Vector3(0, Random.Range(7, -7));
                 crack_instance.transform.Rotate(0, 0, Random.Range(0, 360));
                 crack_instance.transform.localScale = new Vector3(Random.Range(0.5f, 2), Random.Range(0.5f, 2), Random.Range(0.5f, 2));
+                crack_instance.GetComponent<CrackTouch>().crack_has_been_touched = false;
             }
         }
         //cracks = _cracks;
