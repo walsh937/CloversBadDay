7da2810de4417d281add05ef0635fbb588d66992
AndyP <malicioussoap@gmail.com>
2022-11-21 18:18:44 -0600

    unchanged stage commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, 0, -10);
+    }
+}
